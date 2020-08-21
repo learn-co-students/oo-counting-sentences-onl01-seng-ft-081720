20ebf58349d4f70aa9a647367be4442bd6fbf1bc
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-21 03:11:38 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
+
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    sentence_array = self.split(/[.?!]/)
 
+    not_counted = sentence_array.count("")
+    sentence_array.size - not_counted
+    # dont_count = sentence_arraycount('')
   end
+
 end
