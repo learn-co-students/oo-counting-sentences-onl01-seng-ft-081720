91243614631c0dafb8edd18a15e09694155f68bd
ellenupark <ellenupark@gmail.com>
2020-08-21 00:57:09 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.') ? true : false
   end
 
   def question?
-
+    self.end_with?('?') ? true : false
   end
 
   def exclamation?
-
+    self.end_with?('!') ? true : false
   end
 
   def count_sentences
-
+    count = []
+    split = self.downcase.tr('.?!', '=').split('=')
+    split.each do | sentence |
+      if !sentence.empty?
+        count << sentence
+      end
+    end
+    count.length
   end
 end
+
