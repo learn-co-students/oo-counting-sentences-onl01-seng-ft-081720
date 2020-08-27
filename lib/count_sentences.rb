dc46554f35737b8544a2aa6c4e15fd944b397121
mtrimner <matt.trimner@gmail.com>
2020-08-27 10:28:44 -0700

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[.!?]/).compact.reject { |string| string == " " || string == ""}.count
+  
   end
 end
